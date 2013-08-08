ef46555451b70009b8bf4a5e01fdfd502c69489f
Ubuntu <ubuntu@ip-10-204-238-123.ec2.internal>
2013-08-08 05:35:43 +0000

    Bootstrap CSS styling added

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
-var anything = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send(anything);
+    var anything = fs.readFileSync('index.html').toString();
+    response.send(anything);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
